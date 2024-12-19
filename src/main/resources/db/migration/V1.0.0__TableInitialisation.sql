d31d02163510b7757d4c35e47a71205a1d000f35
Yurii <leskiv.yurii@gmail.com>
2024-12-20 01:50:00 +0200

    [Leskiv Yurii] Implemented flyway migration

--- src/main/resources/db/migration/V1.0.0__TableInitialisation.sql
+++ src/main/resources/db/migration/V1.0.0__TableInitialisation.sql
@@ -1,24 +1,18 @@
-CREATE TABLE IF NOT EXISTS EMPLOYEES
+CREATE TABLE IF NOT EXISTS USERS
 (
     id         bigint generated always as identity,
     email      varchar unique not null,
+    role       varchar        not null,
     password   varchar        not null,
     name       varchar        not null,
-    phone      varchar        not null,
-    birth_date date           not null,
+    is_active  boolean        not null,
+    phone      varchar,
+    birth_date date,
+    balance    numeric(10, 2),
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone,
 
-    constraint employee_pk PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS CLIENTS
-(
-    id       bigint generated always as identity,
-    email    varchar unique not null,
-    password varchar        not null,
-    name     varchar        not null,
-    balance  numeric(10, 2) not null,
-
-    constraint client_pk PRIMARY KEY (id)
+    constraint user_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS BOOKS
@@ -31,9 +25,12 @@ CREATE TABLE IF NOT EXISTS BOOKS
     publication_year DATE           not null,
     author           varchar        not null,
     number_of_pages  integer        not null,
+    quantity         integer        not null,
     characteristics  varchar        not null,
     description      varchar        not null,
     language         varchar        not null,
+    created_at       timestamp with time zone,
+    updated_at       timestamp with time zone,
 
     constraint book_pk PRIMARY KEY (id)
 );
@@ -44,12 +41,14 @@ CREATE TABLE IF NOT EXISTS ORDERS
     id          bigint generated always as identity,
     client_id   bigint         not null,
     employee_id bigint         not null,
-    order_date  TIMESTAMP      not null,
+    status      varchar        not null,
     price       numeric(10, 2) not null,
+    created_at  timestamp with time zone,
+    updated_at  timestamp with time zone,
 
     constraint order_pk PRIMARY KEY (id),
-    constraint client_fk FOREIGN KEY (client_id) REFERENCES CLIENTS (id),
-    constraint employee_fk FOREIGN KEY (employee_id) REFERENCES EMPLOYEES (id)
+    constraint client_fk FOREIGN KEY (client_id) REFERENCES USERS (id),
+    constraint employee_fk FOREIGN KEY (employee_id) REFERENCES USERS (id)
 );
 
 CREATE TABLE IF NOT EXISTS "BOOK_ITEMS"
@@ -62,4 +61,16 @@ CREATE TABLE IF NOT EXISTS "BOOK_ITEMS"
     constraint book_item_pk PRIMARY KEY (id),
     constraint order_fk FOREIGN KEY (order_id) REFERENCES ORDERS (id),
     constraint book_fk FOREIGN KEY (book_id) REFERENCES BOOKS (id)
+);
+
+CREATE TABLE IF NOT EXISTS CART_ITEM
+(
+    id bigint generated always as identity,
+    client_id bigint not null,
+    book_id bigint not null,
+    quantity int not null,
+
+    constraint cart_item_pk primary key (id),
+    constraint user_cart_item_fk FOREIGN KEY (client_id) REFERENCES USERS(id),
+    constraint book_cart_item_fk FOREIGN KEY (book_id) REFERENCES BOOKS(id)
 )
